658e79b9b2e3b10c8c152af85180ad79a87a403a
bambip <63555129+bambip@users.noreply.github.com>
2020-06-22 15:02:36 -0500

    project edit

--- objects/objects-101.js
+++ objects/objects-101.js
@@ -3,5 +3,27 @@ let mybook = {
     author: 'George Orwell',
     pageCount: '326'
 }
+//getting single value of the object (properties)
+console.log(`${mybook.title} by ${mybook.author}`)
 
-console.log(mybook)
+mybook.title = 'Animal Farm'
+
+console.log(`${mybook.title} by ${mybook.author}`)
+
+// Challenge
+
+let: person = {
+    name: 'Matthew',
+    age: 21,
+    location: 'Kansas City'
+}
+
+console.log(`${person.name} is ${person.age} and lives in ${person.location}`) 
+
+person.age = person.age + 1
+
+console.log(`${person.name} is ${person.age} and lives in ${person.location}`) 
+
+
+
+ 
